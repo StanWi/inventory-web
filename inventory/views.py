30f13f604408f2b0ff6e3da4db5f364c205f0566
Stan <26521410+StanWi@users.noreply.github.com>
2018-11-16 11:36:41 +0800

    Modified templates to display vendors from database

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Vendor
 
 def post_list(request):
-    return render(request, 'inventory/post_list.html', {})
+    vendors = Vendor.objects.filter(
+        published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'inventory/post_list.html', {'vendors': vendors})
 
