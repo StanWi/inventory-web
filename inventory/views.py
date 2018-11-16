741beecbde8b880b207bcbe973aaf7d5f48cc684
Stan <26521410+StanWi@users.noreply.github.com>
2018-11-16 14:39:39 +0800

    Added view and template for detailed blog post as well as CSS for site

--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Vendor
 
@@ -7,3 +7,6 @@ def post_list(request):
         published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'inventory/post_list.html', {'vendors': vendors})
 
+def vendor_detail(request, pk):
+    vendor = get_object_or_404(Vendor, pk=pk)
+    return render(request, 'inventory/vendor_detail.html', {'vendor': vendor})
